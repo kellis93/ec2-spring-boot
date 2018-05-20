4b12beae9717a6be5f1ee62d4c2a7f4c6444b4d9
kelli <kelli@DESKTOP-6R4PR0G>
2018-05-21 02:07:15 +0900

    ,

---
+++ ec2-spring-boot/src/main/java/com/example/Ec2SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2SpringBootApplication.class, args);
+	}
+}
